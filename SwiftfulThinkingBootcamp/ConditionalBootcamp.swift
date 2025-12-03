4dea9ea52dea94046ffdafc49df40d220d6be92c
Kevin Massol <kevin.massol@icloud.com>
2025-12-03 19:26:34 +0100

    🚀 feat: how to use if-else and conditional statements

---
+++ SwiftfulThinkingBootcamp/ConditionalBootcamp.swift
@@ -0,0 +1,62 @@
+//
+//  ConditionalBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by KEVIN on 03/12/2025.
+//
+
+import SwiftUI
+
+struct ConditionalBootcamp: View {
+    
+    @State var showCircle: Bool = false
+    @State var showRectangle: Bool = false
+    @State var isLoading: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            Button("IS LOADING: \(isLoading.description)") {
+                isLoading.toggle()
+            }
+            
+            if isLoading {
+                ProgressView()
+            }
+            
+//            Button("Circle Button: \(showCircle.description)") {
+//                showCircle.toggle()
+//            }
+//            Button("Rectangle Button: \(showRectangle.description)") {
+//                showRectangle.toggle()
+//            }
+//            
+//            if showCircle {
+//                Circle()
+//                    .frame(width: 100, height: 100)
+//            }
+//            
+//            if showRectangle {
+//                Rectangle()
+//                    .frame(width: 100, height: 100)
+//            }
+//            
+//            if showCircle && showRectangle {
+//                RoundedRectangle(cornerRadius: 25)
+//                    .frame(width: 200, height: 100)
+//            }
+//            
+//            if showCircle || showRectangle {
+//                RoundedRectangle(cornerRadius: 25)
+//                    .frame(width: 200, height: 100)
+//            }
+            
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ConditionalBootcamp()
+}
