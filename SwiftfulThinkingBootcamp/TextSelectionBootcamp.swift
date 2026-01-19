18a0c5fa4a0a0dc71883821d51578a59f1b60a2f
Kevin Massol <kevin.massol@icloud.com>
2026-01-19 21:27:40 +0100

    🚀 feat: how to use TextSelection

---
+++ SwiftfulThinkingBootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by KEVIN on 19/01/2026.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
