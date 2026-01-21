6b4ca6bc118e2e6f1088474c231a90d1896a6f20
Kevin Massol <kevin.massol@icloud.com>
2026-01-21 07:01:36 +0100

    🚀 feat: how to use @FocusState

---
+++ SwiftfulThinkingBootcamp/FocusStateBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FocusStateBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by KEVIN on 21/01/2026.
+//
+
+import SwiftUI
+
+struct FocusStateBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FocusStateBootcamp()
+}
