158b8d60139cc6af4eb43591f1538434886207d8
Kevin Massol <kevin.massol@icloud.com>
2026-01-23 07:31:02 +0100

    🚀 feat: how to use .toolbar in NavigationStack

---
+++ SwiftfulThinkingBootcamp/ToolbarBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ToolbarBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by KEVIN on 23/01/2026.
+//
+
+import SwiftUI
+
+struct ToolbarBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToolbarBootcamp()
+}
