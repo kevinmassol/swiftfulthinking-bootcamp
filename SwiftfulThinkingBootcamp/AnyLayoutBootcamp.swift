4bf7579eff90570d0fbaf00564e5ca3e1e0c6e26
Kevin Massol <kevin.massol@icloud.com>
2026-01-24 11:21:58 +0100

    🚀 feat: how to use AnyLayout

---
+++ SwiftfulThinkingBootcamp/AnyLayoutBootcamp.swift
@@ -0,0 +1,47 @@
+//
+//  AnyLayoutBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by KEVIN on 24/01/2026.
+//
+
+import SwiftUI
+
+struct AnyLayoutBootcamp: View {
+    
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @Environment(\.verticalSizeClass) private var verticalSizeClass
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Horizontal: \(horizontalSizeClass.debugDescription)")
+            Text("Vertical: \(verticalSizeClass.debugDescription)")
+            
+            let layout: AnyLayout = horizontalSizeClass == .compact ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
+            
+            layout {
+                Text("Alpha")
+                Text("Beta")
+                Text("Gamma")
+            }
+            
+//            if horizontalSizeClass == .compact {
+//                VStack {
+//                    Text("Alpha")
+//                    Text("Beta")
+//                    Text("Gamma")
+//                }
+//            } else {
+//                HStack {
+//                    Text("Alpha")
+//                    Text("Beta")
+//                    Text("Gamma")
+//                }
+//            }
+        }
+    }
+}
+
+#Preview {
+    AnyLayoutBootcamp()
+}
