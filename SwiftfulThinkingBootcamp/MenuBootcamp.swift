d6e509800a52b0f02fcd8d0595a721b235101d06
Kevin Massol <kevin.massol@icloud.com>
2026-01-24 10:40:13 +0100

    🚀 feat: how to use Menu to replace Picker

---
+++ SwiftfulThinkingBootcamp/MenuBootcamp.swift
@@ -0,0 +1,31 @@
+//
+//  MenuBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by KEVIN on 24/01/2026.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Click Me") {
+            Button("One") {
+                //
+            }
+            Button("Two") {
+                //
+            }
+            Button("Three") {
+                //
+            }
+            Button("Four") {
+                //
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuBootcamp()
+}
