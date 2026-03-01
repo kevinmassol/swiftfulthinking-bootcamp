faa16e844167859309094bacb2fa2e333f10fbe6
Kevin Massol <kevin.massol@icloud.com>
2026-03-01 15:34:20 +0100

    🖌️ refactor: change the name of the project

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by KEVIN on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcamp: App {
+    var body: some Scene {
+        WindowGroup {
+            SubmitTextFieldBootcamp()
+        }
+    }
+}
